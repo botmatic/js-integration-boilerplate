3b35ff412b33bb301a5130a0982339c685eab1c8
kévin <quesada.kevin@gmail.com>
2018-02-14 15:05:57 +0100

    add debug

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const botmatic = require('@botmatic/js-integration')()
+const botmatic = require('@botmatic/js-integration')({
+  auth: (token) => {
+    return Promise.resolve({success: token=="azertyuiopqsdfghjklmwxcvbn"})
+  }
+})
 
 // Tips: you can use regexp for action name.
 botmatic.onAction(".*", ({client, data}) => {
