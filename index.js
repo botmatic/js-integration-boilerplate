d64d626c1f508e5aa102990feb1840ec246a961f
kévin <quesada.kevin@gmail.com>
2018-02-14 14:47:16 +0100

    test

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const botmatic = require('@botmatic/js-integration')()
 
 // Tips: you can use regexp for action name.
 botmatic.onAction(".*", ({client, data}) => {
+  console.log('RECEIVE ACTION')
   return Promise.resolve({data: {action: data.action, type:"action"}});
 })
