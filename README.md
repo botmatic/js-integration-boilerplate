9ef4a417dc77f65159b2e5fd48bea0f64274dbfe
kévin <quesada.kevin@gmail.com>
2018-03-08 17:15:24 +0100

    update comments

--- README.md
+++ README.md
@@ -40,12 +40,6 @@ Explain how to run the automated tests for this system
 
 Add additional notes about how to deploy this on a live system
 
-## Authors
-
-* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
-
-See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
-
 ## License
 
 This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ const Mustache = require('mustache')
 const tplFieldsBuf = fs.readFileSync(__dirname + '/../views/fields.html');
 const tplFieldsStr = tplFieldsBuf.toString('utf8')
 
+// Import .env
+// Don't forget to create .env file at root.
 require('dotenv').config({
   path: path.join(__dirname, '/../.env')
 })
 
+// Construct botmatic client.
 const botmatic = require('@botmatic/js-integration')({
   server: app,
   auth: (token) => {
@@ -26,11 +29,13 @@ const botmatic = require('@botmatic/js-integration')({
   }
 })
 
+// Listen actions.
 // Tips: you can use regexp for action name.
 botmatic.onAction(".*", ({client, data}) => {
   return Promise.resolve({data: {action: data.action, type:"action"}});
 })
 
+// Listen events.
 // Tips: you can use regexp for event name.
 botmatic.onEvent(botmatic.events.CONTACT_UPDATED, function({client, data}) {
   return new Promise((resolve, reject) => {
@@ -78,4 +83,5 @@ botmatic.onUpdateSettings('/settingspath', function(token, data) {
   })
 })
 
+// Start express server.
 var listener = app.listen(process.env.BOTMATIC_PORT, () => console.log(`App listening on port ${listener.address().port}!`))
