7d4c3dba4323577ac67dc77863ab962947e41c3d
kévin <quesada.kevin@gmail.com>
2018-03-23 08:24:02 +0100

    set properties with new sdk

--- src/index.js
+++ src/index.js
@@ -1,87 +1,33 @@
-var express = require('express');
-var app = express();
-const path = require('path')
+const botmatic = require('@botmatic/js-integration')({port: 5050})
 
-// Used for integration settings page.
-const fs = require('fs');
-const Mustache = require('mustache')
-const tplFieldsBuf = fs.readFileSync(__dirname + '/../views/fields.html');
-const tplFieldsStr = tplFieldsBuf.toString('utf8')
-
-// Import .env
-// Don't forget to create .env file at root.
-require('dotenv').config({
-  path: path.join(__dirname, '/../.env')
-})
-
-// Construct botmatic client.
-const botmatic = require('@botmatic/js-integration')({
-  server: app,
-  auth: (token) => {
-    return new Promise((resolve, reject) => {
-      // Retrieve the client in your database, or other.
-      const client_authenticated = {id: "client_id"}
-      // If the client is known
-      resolve(client_authenticated)
-      // if not
-      // reject();
-    })
-  }
-})
-
-// Listen actions.
-// Tips: you can use regexp for action name.
 botmatic.onAction(".*", ({client, data}) => {
-  return Promise.resolve({data: {action: data.action, type:"action"}});
+  console.log('RECEIVE ACTION')
+  console.log(data)
+  // return new Promise((resolve, reject) => {
+    return updateBotmaticContactProperties(body.data.contact_id, body.action)
+  // })
 })
 
-// Listen events.
-// Tips: you can use regexp for event name.
-botmatic.onEvent(botmatic.events.CONTACT_UPDATED, function({client, data}) {
+const updateBotmaticContactProperties = (contact_id, action) => {
+  const prop = action.split("_");
+
   return new Promise((resolve, reject) => {
-    resolve({data: "ok", type: "data"});
-  })
-})
+    if (prop.length == 2) {
+      let dataToSend = {contact : {}};
+      dataToSend.contact[prop[0]] = prop[1];
 
-/**
- * Settings form integration page.
- */
-botmatic.onSettingsPage("/settingspath", (token) => {
-  return new Promise(async (resolve) => {
-    const myObject = {api_key: "my api key"}
+      var url = process.env.BOTMATIC_URL + 'api/contacts/'+contact_id;
+      var headers = {
+          'Authorization': 'Bearer ' + process.env.BOTMATIC_WORKSPACE_TOKEN
+      };
 
-    // Construct the html template to return.
-    // The second parameter is an object with:
-    // - name: name value of html input tag.
-    // - value: value when form is in modification.
-    let tpl = Mustache.render(tplFieldsStr, {name: "api_key", value: myObject.api_key});
-    resolve(tpl)
-  })
-})
-
-/**
- * When Botmatic integration form is submitted, update datastore with client.
- * @param  {String} token Botmatic integration token
- * @param  {Object} data  Content the event
- * @return {Promise} Always resolvev promise, with success= true or false
- */
-botmatic.onUpdateSettings('/settingspath', function(token, data) {
-  return new Promise(async (resolve) => {
-    // Check if API key is given.
-    if (data.api_key) {
-      // Store value somewhere.
-      resolve({success: true})
+      request.patch({ url: url, form: dataToSend, headers: headers }, function (e, r, body) {
+        console.log("body)")
+        console.log(body)
+        resolve({success: true, data: {}, type: "data"});
+      });
     } else {
-      console.error('ERROR:', "API key is required.");
-      resolve({
-        success: false,
-        errorFields: {
-          api_key: "Field required"
-        }
-      })
+      reject({success: false, data: {error: "Property not found"}, type: "data"});
     }
-  })
-})
-
-// Start express server.
-var listener = app.listen(process.env.BOTMATIC_PORT, () => console.log(`App listening on port ${listener.address().port}!`))
+  });
+}
