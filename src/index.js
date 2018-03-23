8399deda0e16af994f115549e30a0fcb42ea972c
kévin <quesada.kevin@gmail.com>
2018-03-23 08:45:20 +0100

    add try catch

--- src/index.js
+++ src/index.js
@@ -21,32 +21,37 @@ const updateBotmaticContactProperties = (contact_id, action) => {
   const prop = action.split("_");
 
   return new Promise((resolve, reject) => {
-    if (prop.length == 2) {
-      console.log('good format for action name')
+    try {
+      if (prop.length == 2) {
+        console.log('good format for action name')
 
-      let dataToSend = {contact : {}};
-      dataToSend.contact[prop[0]] = prop[1];
+        let dataToSend = {contact : {}};
+        dataToSend.contact[prop[0]] = prop[1];
 
-      var url = process.env.BOTMATIC_URL + 'api/contacts/'+contact_id;
-      var headers = {
-          'Authorization': 'Bearer ' + process.env.BOTMATIC_WORKSPACE_TOKEN
-      };
+        var url = process.env.BOTMATIC_URL + 'api/contacts/'+contact_id;
+        var headers = {
+            'Authorization': 'Bearer ' + process.env.BOTMATIC_WORKSPACE_TOKEN
+        };
 
-      console.log('headers')
-      console.log(headers)
+        console.log('headers')
+        console.log(headers)
 
-      request.patch({ url: url, form: dataToSend, headers: headers }, function (e, r, body) {
-        console.log("body)")
-        console.log(body)
+        request.patch({ url: url, form: dataToSend, headers: headers }, function (e, r, body) {
+          console.log("body)")
+          console.log(body)
 
-        console.log("e)")
-        console.log(e)
+          console.log("e)")
+          console.log(e)
 
-        resolve({success: true, data: {}, type: "data"});
-      });
-    } else {
-      console.log('reject, bad format for action name')
-      reject({success: false, data: {error: "Property not found"}, type: "data"});
+          resolve({success: true, data: {}, type: "data"});
+        });
+      } else {
+        console.log('reject, bad format for action name')
+        reject({success: false, data: {error: "Property not found"}, type: "data"});
+      }
+    } catch(e) {
+      console.log('ERROR')
+      console.log(e)
     }
   });
 }
